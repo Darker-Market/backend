fcf842ab62ba9818a20ffe7d2704c1207f4093d8
Gavin Jin <gavinjin62@gmail.com>
2025-07-12 23:19:27 -0700

    query items from darkerApi and response to frontend

--- src/routes/testRoute.ts
+++ src/routes/testRoute.ts
@@ -1,10 +1,18 @@
 import { Router, Request, Response } from "express";
+import axios from "axios";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   try {
-    res.json("test !!!!!!!!!");
+    const apiKey = process.env.DARKER_API;
+    const externalUrl = `https://api.darkerdb.com/v1/market?key=${apiKey}&condense=true&limit=5`;
+
+    const response = await axios.get(externalUrl);
+
+    console.log(response.data);
+
+    res.json(response.data);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch " });
